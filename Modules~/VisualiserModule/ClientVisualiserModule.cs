f47a1c2a0238be1dfeea276eeb32913a4b410714
Christoph Holtmann <christoph@holtmann.world>
2023-07-03 18:55:21 +0200

    feat: improve client visualiser module example

--- Modules~/VisualiserModule/ClientVisualiserModule.cs
+++ Modules~/VisualiserModule/ClientVisualiserModule.cs
@@ -1,7 +1,6 @@
 using CENTIS.UnityModuledNet.Networking;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using UnityEngine;
 using CENTIS.UnityModuledNet.Managing;
 
@@ -13,163 +12,183 @@ namespace CENTIS.UnityModuledNet.Modules
 {
     public class ClientVisualiserModule : UnreliableModule
     {
-		public override string ModuleID => "ClientVisualiserModule";
+        public override string ModuleID => "ClientVisualiserModule";
 
-		private const string PARENT_NAME = "ClientVisualiser Parent";
-		private readonly Dictionary<byte, ClientVisualiser> _visualisers = new();
+        private const string PARENT_NAME = "ClientVisualiser Parent";
+        private readonly Dictionary<byte, ClientVisualiser> _visualisers = new();
 
-		private Vector3 _lastCameraPosition;
-		private Vector3 _lastCameraRotation;
-		private int _clientVisualiserDelay = ClientVisualiserSettings.Settings.ClientVisualiserDelay;
+        private Vector3 _lastCameraPosition;
+        private Vector3 _lastCameraRotation;
+        private int _clientVisualiserDelay = ClientVisualiserSettings.Settings.SyncDelay;
 
-		private Transform _visualiserParent;
+        private Transform _visualiserParent;
 
-		#region lifecycle
+        #region lifecycle
 
-		public ClientVisualiserModule()
-		{
-			ModuledNetManager.OnConnected += CreateVisualiserParent;
-			ModuledNetManager.OnDisconnected += DestroyVisualiserParent;
-			ModuledNetManager.OnClientDisconnected += RemoveConnectedClient;
-		}
+        public ClientVisualiserModule()
+        {
+            ModuledNetManager.OnConnected += CreateVisualiserParent;
+            ModuledNetManager.OnDisconnected += DestroyVisualiserParent;
+            ModuledNetManager.OnClientDisconnected += RemoveConnectedClient;
+        }
 
-		public override void Dispose()
-		{
-			ModuledNetManager.OnConnected -= CreateVisualiserParent;
-			ModuledNetManager.OnDisconnected -= DestroyVisualiserParent;
-			ModuledNetManager.OnClientDisconnected -= RemoveConnectedClient;
+        public override void Dispose()
+        {
+            ModuledNetManager.OnConnected -= CreateVisualiserParent;
+            ModuledNetManager.OnDisconnected -= DestroyVisualiserParent;
+            ModuledNetManager.OnClientDisconnected -= RemoveConnectedClient;
 
-			if (_visualiserParent != null)
-				DestroyVisualiserParent();
-			base.Dispose();
-		}
+            if (_visualiserParent != null)
+                DestroyVisualiserParent();
+            base.Dispose();
+        }
 
-		public override void Update()
-		{
-			_clientVisualiserDelay++;
+        public override void Update()
+        {
+            _clientVisualiserDelay++;
 #if UNITY_EDITOR
-			if (SceneView.lastActiveSceneView.camera.transform.hasChanged)
-				CurrentCameraMoved(SceneView.lastActiveSceneView.camera.transform);
+            if (SceneView.lastActiveSceneView.camera.transform.hasChanged)
+                CurrentCameraMoved(SceneView.lastActiveSceneView.camera.transform);
 #else
 			if (Camera.current.transform && Camera.current.transform.hasChanged)
 				CurrentCameraMoved(Camera.current.transform);
 #endif
-		}
+        }
 
-		private void CreateVisualiserParent()
-		{
-			GameObject parent = GameObject.Find(PARENT_NAME);
-			_visualiserParent = parent == null ? new GameObject().transform : parent.transform;
-			_visualiserParent.name = PARENT_NAME;
-		}
+        private void CreateVisualiserParent()
+        {
+            GameObject parent = GameObject.Find(PARENT_NAME);
+            _visualiserParent = parent == null ? new GameObject().transform : parent.transform;
+            _visualiserParent.name = PARENT_NAME;
+        }
 
-		private void DestroyVisualiserParent()
-		{
+        private void DestroyVisualiserParent()
+        {
+            if (_visualiserParent == null) return;
+
 #if UNITY_EDITOR
-			GameObject.DestroyImmediate(_visualiserParent?.gameObject);
+            GameObject.DestroyImmediate(_visualiserParent?.gameObject);
 #else
 			GameObject.Destroy(_visualiserParent?.gameObject);
 #endif
-		}
+            _visualiserParent = null;
+        }
 
-		private void RemoveConnectedClient(byte id)
-		{
-			if (!_visualisers.Remove(id, out ClientVisualiser visualiser))
-				return;
+        public void FixOrphanedVisualiser()
+        {
+            if (_visualiserParent != null) DestroyVisualiserParent();
+            else
+            {
+                _visualiserParent = GameObject.FindObjectOfType<ClientVisualiser>()?.transform.parent;
+                DestroyVisualiserParent();
+            }
+        }
 
+        private void RemoveConnectedClient(byte id)
+        {
+            if (!_visualisers.Remove(id, out ClientVisualiser visualiser))
+                return;
+
+            if (visualiser == null) return;
 #if UNITY_EDITOR
-			GameObject.DestroyImmediate(visualiser.gameObject);
+            GameObject.DestroyImmediate(visualiser.gameObject);
 #else
 			GameObject.Destroy(visualiser.gameObject);
 #endif
-		}
+            visualiser = null;
+        }
 
-		#endregion
+        #endregion
 
-		#region receive data
+        #region receive data
 
-		public override void OnReceiveData(byte sender, byte[] data)
-		{
-			if (!ModuledNetManager.ConnectedClients.TryGetValue(sender, out ClientInformation client))
-				return;
+        public override void OnReceiveData(byte sender, byte[] data)
+        {
+            if (!ModuledNetManager.ConnectedClients.TryGetValue(sender, out ClientInformation client))
+                return;
 
-			if (ModuledNetManager.IsDebug)
-				Debug.Log($"Received Camera Update from {client}");
+            if (ModuledNetManager.IsDebug)
+                Debug.Log($"Received Camera Update from {client}");
 
-			int size = sizeof(float);
-			byte[] positionX = GetBytesFromArray(data, 0 * size, size);
-			byte[] positionY = GetBytesFromArray(data, 1 * size, size);
-			byte[] positionZ = GetBytesFromArray(data, 2 * size, size);
-			byte[] rotationX = GetBytesFromArray(data, 3 * size, size);
-			byte[] rotationY = GetBytesFromArray(data, 4 * size, size);
-			byte[] rotationZ = GetBytesFromArray(data, 5 * size, size);
+            int size = sizeof(float);
+            byte[] positionX = GetBytesFromArray(data, 0 * size, size);
+            byte[] positionY = GetBytesFromArray(data, 1 * size, size);
+            byte[] positionZ = GetBytesFromArray(data, 2 * size, size);
+            byte[] rotationX = GetBytesFromArray(data, 3 * size, size);
+            byte[] rotationY = GetBytesFromArray(data, 4 * size, size);
+            byte[] rotationZ = GetBytesFromArray(data, 5 * size, size);
 
-			if (!_visualisers.TryGetValue(sender, out ClientVisualiser visualiser))
-			{
-				GameObject obj = GameObject.Instantiate(ClientVisualiserSettings.Settings.ClientVisualiser.gameObject, _visualiserParent);
-				visualiser = obj.GetComponent<ClientVisualiser>();
-				visualiser.UpdateVisualiser(client.ID, client.Username, client.Color);
-				_visualisers.Add(sender, visualiser);
-			}
+            if (!_visualisers.TryGetValue(sender, out ClientVisualiser visualiser))
+            {
+                GameObject obj = GameObject.Instantiate(ClientVisualiserSettings.Settings.ClientVisualiser.gameObject, _visualiserParent);
+                visualiser = obj.GetComponent<ClientVisualiser>();
+                visualiser.UpdateVisualiser(client.ID, client.Username, client.Color);
+                _visualisers.Add(sender, visualiser);
+            }
 
-			visualiser.transform.position = new Vector3(BitConverter.ToSingle(positionX),
-				BitConverter.ToSingle(positionY),
-				BitConverter.ToSingle(positionZ));
-			visualiser.transform.eulerAngles = new Vector3(BitConverter.ToSingle(rotationX),
-				BitConverter.ToSingle(rotationY),
-				BitConverter.ToSingle(rotationZ));
-			if (!visualiser.gameObject.activeSelf)
-				visualiser.gameObject.SetActive(true);
-		}
+            if (visualiser)
+            {
+                var position = new Vector3(BitConverter.ToSingle(positionX),
+                    BitConverter.ToSingle(positionY),
+                    BitConverter.ToSingle(positionZ));
+                var rotation = new Vector3(BitConverter.ToSingle(rotationX),
+                    BitConverter.ToSingle(rotationY),
+                    BitConverter.ToSingle(rotationZ));
+                visualiser.SetTransform(position, rotation, ClientVisualiserSettings.Settings.Lerp);
 
-		#endregion
+                if (!visualiser.gameObject.activeSelf)
+                    visualiser.gameObject.SetActive(true);
+            }
+        }
 
-		#region send data
+        #endregion
 
-		private void CurrentCameraMoved(Transform camera)
-		{
-			// limit camera syncs
-			if (_clientVisualiserDelay < ClientVisualiserSettings.Settings.ClientVisualiserDelay)
-			{
-				camera.hasChanged = false;
-				return;
-			}
+        #region send data
 
-			if (_lastCameraPosition != null
-				&& camera.position.Equals(_lastCameraPosition)
-				&& camera.eulerAngles.Equals(_lastCameraRotation))
-			{
-				camera.hasChanged = false;
-				return;
-			}
+        private void CurrentCameraMoved(Transform camera)
+        {
+            // limit camera syncs
+            if (_clientVisualiserDelay < ClientVisualiserSettings.Settings.SyncDelay)
+            {
+                camera.hasChanged = false;
+                return;
+            }
 
-			// TODO : combine de-/serializer
-			// TODO : optimize this
-			byte[] data = new byte[sizeof(float) * 6];
-			Array.Copy(BitConverter.GetBytes(camera.position.x), 0, data, 0 * sizeof(float), sizeof(float));
-			Array.Copy(BitConverter.GetBytes(camera.position.y), 0, data, 1 * sizeof(float), sizeof(float));
-			Array.Copy(BitConverter.GetBytes(camera.position.z), 0, data, 2 * sizeof(float), sizeof(float));
-			Array.Copy(BitConverter.GetBytes(camera.eulerAngles.x), 0, data, 3 * sizeof(float), sizeof(float));
-			Array.Copy(BitConverter.GetBytes(camera.eulerAngles.y), 0, data, 4 * sizeof(float), sizeof(float));
-			Array.Copy(BitConverter.GetBytes(camera.eulerAngles.z), 0, data, 5 * sizeof(float), sizeof(float));
-			SendData(data, null);
+            if (_lastCameraPosition != null
+                && camera.position.Equals(_lastCameraPosition)
+                && camera.eulerAngles.Equals(_lastCameraRotation))
+            {
+                camera.hasChanged = false;
+                return;
+            }
 
-			_lastCameraPosition = camera.position;
-			_lastCameraRotation = camera.eulerAngles;
-			camera.hasChanged = false;
-			_clientVisualiserDelay = 0;
-		}
+            // TODO : combine de-/serializer
+            // TODO : optimize this
+            byte[] data = new byte[sizeof(float) * 6];
+            Array.Copy(BitConverter.GetBytes(camera.position.x), 0, data, 0 * sizeof(float), sizeof(float));
+            Array.Copy(BitConverter.GetBytes(camera.position.y), 0, data, 1 * sizeof(float), sizeof(float));
+            Array.Copy(BitConverter.GetBytes(camera.position.z), 0, data, 2 * sizeof(float), sizeof(float));
+            Array.Copy(BitConverter.GetBytes(camera.eulerAngles.x), 0, data, 3 * sizeof(float), sizeof(float));
+            Array.Copy(BitConverter.GetBytes(camera.eulerAngles.y), 0, data, 4 * sizeof(float), sizeof(float));
+            Array.Copy(BitConverter.GetBytes(camera.eulerAngles.z), 0, data, 5 * sizeof(float), sizeof(float));
+            SendData(data, null);
 
-		private static byte[] GetBytesFromArray(byte[] array, int offset, int size = 0)
-		{
-			if (size == 0)
-				size = array.Length - offset;
+            _lastCameraPosition = camera.position;
+            _lastCameraRotation = camera.eulerAngles;
+            camera.hasChanged = false;
+            _clientVisualiserDelay = 0;
+        }
 
-			byte[] bytes = new byte[size];
-			Array.Copy(array, offset, bytes, 0, size);
-			return bytes;
-		}
+        private static byte[] GetBytesFromArray(byte[] array, int offset, int size = 0)
+        {
+            if (size == 0)
+                size = array.Length - offset;
 
-		#endregion
-	}
+            byte[] bytes = new byte[size];
+            Array.Copy(array, offset, bytes, 0, size);
+            return bytes;
+        }
+
+        #endregion
+    }
 }
--- Modules~/VisualiserModule/ClientVisualiserSettings.cs
+++ Modules~/VisualiserModule/ClientVisualiserSettings.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using CENTIS.UnityModuledNet.Utilities;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -10,41 +10,64 @@ namespace CENTIS.UnityModuledNet.Modules
 #if UNITY_EDITOR
     [InitializeOnLoad()]
 #endif
-    public class ClientVisualiserSettings : ModuleSettings
+    public class ClientVisualiserSettings : ModuleSettings<ClientVisualiserModule>
     {
         private static ClientVisualiserSettings _settings;
 
         public static ClientVisualiserSettings Settings
-		{
-			get
-			{
+        {
+            get
+            {
                 if (_settings == null)
                     _settings = ModuledNetSettings.GetOrCreateSettings<ClientVisualiserSettings>("ClientVisualiser");
                 return _settings;
-			}
-		}
+            }
+        }
 
         protected override string SettingsName => "ClientVisualiserSettings";
 
-        public int ClientVisualiserDelay = 100;
-        public ClientVisualiser ClientVisualiser;
-        public Material ClientVisualiserMaterial;
+        public int SyncDelay = 100;
+        public ClientVisualiser ClientVisualiser = null;
+        public Material Material = null;
+        public bool Lerp = false;
 
         static ClientVisualiserSettings()
         {
 #if UNITY_EDITOR
             AssemblyReloadEvents.afterAssemblyReload += () =>
-                _settings = ModuledNetSettings.GetOrCreateSettings<ClientVisualiserSettings>("ClientVisualiser");
+            {
+                Assert.IsNotNull(Settings);
+            };
+#endif
+        }
+        private ClientVisualiserSettings()
+        {
+#if UNITY_EDITOR
+            AssemblyReloadEvents.afterAssemblyReload += () =>
+            {
+                Module.FixOrphanedVisualiser();
+            };
 #endif
         }
 
-		protected override void DrawModuleSettings()
-		{
+        protected override void DrawModuleSettings()
+        {
 #if UNITY_EDITOR
-            Settings.ClientVisualiserDelay = EditorGUILayout.IntField("ClientVisualiser Delay:", Settings.ClientVisualiserDelay);
-            Settings.ClientVisualiser = (ClientVisualiser)EditorGUILayout.ObjectField("ClientVisualiser Prefab:", Settings.ClientVisualiser, typeof(ClientVisualiser), false);
-            Settings.ClientVisualiserMaterial = (Material)EditorGUILayout.ObjectField("ClientVisualiser Material:", Settings.ClientVisualiserMaterial, typeof(Material), false);
+            CheckForClientVisualizer();
+
+            Settings.SyncDelay = EditorGUILayout.IntField("Delay", Settings.SyncDelay);
+            Settings.ClientVisualiser = (ClientVisualiser)EditorGUILayout.ObjectField("Prefab", Settings.ClientVisualiser, typeof(ClientVisualiser), false);
+            Settings.Material = (Material)EditorGUILayout.ObjectField("Material", Settings.Material, typeof(Material), false);
+            Settings.Lerp = EditorGUILayout.Toggle("Lerp Movement (buggy)", Settings.Lerp);
 #endif
+        }
+
+        private void CheckForClientVisualizer()
+        {
+            if (ClientVisualiser == null)
+                ClientVisualiser = AssetUtilities.FindPrefabAssetInProjectFolder<ClientVisualiser>("ClientVisualiser");
+            if (Material == null)
+                Material = AssetUtilities.FindMaterialAssetInProjectFolder("ClientVisualiser");
         }
     }
 }
